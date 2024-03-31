41181dce74455cb8f7755495a041ed862cc6fa56
f1rstf1y9 <f1rstf1y9@naver.com>
2024-03-31 20:00:14 +0900

    init: project setup

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAISP8KyovJOneEoRUb7Yq6uPH8ZbYBQQ0",
+  authDomain: "your-music-buddy.firebaseapp.com",
+  projectId: "your-music-buddy",
+  storageBucket: "your-music-buddy.appspot.com",
+  messagingSenderId: "826298110919",
+  appId: "1:826298110919:web:f3fbec0337a450ff19bfe2",
+  measurementId: "G-T64GR6E703",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
